80b26ed4398b3c93ea9bad8d42023cc96ffc2e36
James Ho <james687@gmail.com>
2017-08-08 14:15:18 +0800

    start Django project - introduce bootstrap - settings package with local, base, and production settings - add app "tweets"     - first model Tweet

---
+++ tweets/models.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.db import models
+
+class Tweet(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    content = models.CharField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.content)
---
+++ tweets/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def tweet_detail_view(request, id=1):
+    return render(request, 'tweets/detail_view.html', {})
+
+def tweet_list_view(request):
+    return render(request, 'tweets/list_view.html', {})
