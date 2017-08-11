0af12fb3e557b38b0bd6d6a9914856b82a688a69
James Ho <james687@gmail.com>
2017-08-11 19:56:01 +0800

    use custom tweet form for admin

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
+from .forms import TweetModelForm
 from .models import Tweet
 
-admin.site.register(Tweet)
+class TweetModelAdmin(admin.ModelAdmin):
+    form = TweetModelForm
+
+admin.site.register(Tweet, TweetModelAdmin)
