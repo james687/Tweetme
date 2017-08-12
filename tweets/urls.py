4d6dbac2d583d07df5bbebc2d6c3004a31b1bd21
James Ho <james687@gmail.com>
2017-08-12 21:16:50 +0800

    DeleteView

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from .views import TweetList, TweetDetail, TweetCreate, TweetUpdate
+from .views import TweetList, TweetDetail, TweetCreate, TweetUpdate, TweetDelete
 
 urlpatterns = [
     url(r'^$', TweetList.as_view(), name='list'),
     url(r'^(?P<pk>\d+)/$', TweetDetail.as_view(), name='detail'),
     url(r'create/', TweetCreate.as_view(), name='create'),
     url(r'^(?P<pk>\d+)/edit/$', TweetUpdate.as_view(), name='update'),
+    url(r'^(?P<pk>\d+)/delete/$', TweetDelete.as_view(), name='delete'),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .mixins import FormUserNeededMixin, UserOwnerMixin
 from .forms import TweetModelForm
@@ -32,6 +33,13 @@ class TweetUpdate(LoginRequiredMixin, UserOwnerMixin, UpdateView):
     form_class = TweetModelForm
     template_name = "update.html"
     login_url = '/admin/'
+
+
+class TweetDelete(LoginRequiredMixin, DeleteView):
+    model = Tweet
+    template_name = 'delete_confirm.html'
+    success_url = reverse_lazy('tweet:list')
+    # success_url = '/tweets/' # this will do the same thing as above
 
 # function-based view for CreateView with user set to the current user
 
