f1592f7d69c369c00d5520590d35774e30f6bac5
James Ho <james687@gmail.com>
2017-08-24 20:18:33 +0800

    show followers and followed by on user detail page - create Model UserProfile

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile')
+    following = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='followed_by')
+
+    def __str__(self):
+        return '%s(following %d user(s))' % (self.user.username, self.following.all().count())
+
+    def get_absolute_url(self):
+        return reverse('users:detail', kwargs={'username': self.user.username})
